297ae374f586929841ca94df39ce93b23e12debd
huangyafei <huangyafei@sail-cloud.com>
2023-05-09 16:17:06 +0800

    refactor: :recycle: learna + pnpm monorepo 改造

---
+++ template/virtual-list/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import VirtualList from 'virtual-list'
+
+import { ref } from 'vue';
+
+const data = ref([])
+
+const arr = []
+for (let index = 0; index < 100000; index++) {
+  arr.push({
+    label: `item ${index}`,
+    value: index
+  })
+}
+data.value = arr
+</script>
+
+<template>
+  <VirtualList :data="data" style="height: 500px" :scrollToIndex="10000">
+    <template #default="{ item }">
+      <div>{{ item.label }}</div>
+    </template>
+  </VirtualList>
+</template>
+
+<style scoped>
+</style>
